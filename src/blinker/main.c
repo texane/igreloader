a90cb57f5acc57ffe5230c6afbd07a57b545e2c3
Fabien Le Mentec <texane@gmail.com>
2012-01-11 12:41:34 -0600

    [update] blinker sample project, test interrupts

--- src/blinker/main.c
+++ src/blinker/main.c
@@ -1,5 +1,7 @@
 #include <p33Fxxxx.h>
-/* #include "bootloader.h" */
+
+
+#define CONFIG_USE_INTERRUPT 1
 
 
 /* configuration bits */
@@ -10,26 +12,69 @@ _FWDT(FWDTEN_OFF);
 _FBS(BWRP_WRPROTECT_OFF);
 
 
+/* timer interrupt. test if it is written correcly. */
+
+static unsigned char x = 0;
+static unsigned char a = 0; 
+
+void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void)
+{
+  if (IFS0bits.T1IF == 0) return ;
+
+  if (((++a) & 3) == 0)
+  {
+    PORTAbits.RA0 = x & 1;
+    x ^= 1;
+  }
+
+  IFS0bits.T1IF = 0;
+}
+
+#define CONFIG_OSC_FOSC 79227500
+#define OSC_FCY (CONFIG_OSC_FOSC / 2)
+
+static void osc_setup(void)
+{
+  /* fast rc oscillator */
+
+  PLLFBD = 41;
+  CLKDIVbits.PLLPOST = 0;
+  CLKDIVbits.PLLPRE = 0;
+
+  OSCTUN = 0;
+  RCONbits.SWDTEN = 0;
+
+  __builtin_write_OSCCONH(0x01);
+  __builtin_write_OSCCONL(0x01);
+  while (OSCCONbits.COSC != 1) ;
+  while (OSCCONbits.LOCK != 1) ;
+}
+
 int main(void)
 {
-  register unsigned int i;
-  register unsigned int j;
+  AD1PCFGL = 0xFFFF;
+
+  osc_setup();
 
   TRISAbits.TRISA0 = 0;
 
- redo:
-
-  for (i = 0; i < 10000; ++i)
-    for (j = 0; j < 1000; ++j)
-      asm volatile ("nop");
-  PORTAbits.RA0 = 0;
-
-  for (i = 0; i < 10000; ++i)
-    for (j = 0; j < 1000; ++j)
-      asm volatile ("nop");
   PORTAbits.RA0 = 1;
 
-  goto redo;
+  /* timer0 configured in timer mode */
+  IEC0bits.T1IE = 0;
+  T1CONbits.TON = 0;
+  IFS0bits.T1IF = 0;
+  IEC0bits.T1IE = 1;
+
+  /* clocked from FCY / prescaler (256) */
+  T1CONbits.TCKPS = 3;
+  TMR1 = 0x0000;
+  PR1 = 0xffff;
+
+  /* start the timer */
+  T1CONbits.TON = 1;
+
+  while (1) ;
 
   return 0;
 }
