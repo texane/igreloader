edbbbb54a5201bd1a989d6232410d133da0befa2
Fabien Le Mentec <texane@gmail.com>
2012-01-13 08:57:12 -0600

    [update] add ECAN to device

--- src/device/main.c
+++ src/device/main.c
@@ -109,21 +109,159 @@ static inline void delay(void)
 
 /* communication layer */
 
-#define CONFIG_USE_ECAN 0
+#define CONFIG_USE_ECAN 1
 
 #if CONFIG_USE_ECAN
 
+uint16_t ecan_bufs[4][8] __attribute__((space(dma), aligned(4 * 16)));
+
 static void ecan_setup(uint8_t id)
 {
-  /* todo: use id as a boot command filter */
+  /* baud rate */
+#define FCAN 40000000 
+#define BITRATE 125000  
+#define NTQ 10
+#define BRP_VAL ((FCAN / (2 * NTQ * BITRATE)) - 1)
+
+  /* setup dma channel 0 for tx buffer */
+  DMACS0 = 0;
+  DMA0CON = 0x2020; 
+  DMA0PAD = 0x0442;
+  DMA0CNT = 7;
+  DMA0REQ = 0x0046;	
+  DMA0STA = __builtin_dmaoffset(&ecan_bufs);
+  DMA0CONbits.CHEN = 1;
+
+  /* setup dma channel 2 for rx buffer */
+  DMACS0 = 0;
+  DMA2CON = 0x0020;
+  DMA2PAD = 0x0440;	
+  DMA2CNT = 7;
+  DMA2REQ = 0x0022;	
+  DMA2STA = __builtin_dmaoffset(&ecan_bufs);
+  DMA2CONbits.CHEN = 1;
+
+  /* setup pins */
+  /* c1rx mapped to rb6 */
+  /* c1tx mapped to rb7. refer to table 11-2. */
+  RPINR26bits.C1RXR = 6;
+  RPOR3bits.RP7R = 0x10;
+  TRISBbits.TRISB6 = 1;
+  TRISBbits.TRISB7 = 0;
+
+  /* configuration mode */
+  C1CTRL1bits.REQOP = 4;
+  while (C1CTRL1bits.OPMODE != 4);
+
+  /* FCAN is selected to be FCY */
+  C1CTRL1bits.CANCKS = 0x1;
+
+  C1CFG1bits.BRP = BRP_VAL;
+  C1CFG1bits.SJW = 0x1;
+  C1CFG2bits.SEG1PH = 0x2;
+  C1CFG2bits.SEG2PHTS = 0x1;
+  C1CFG2bits.SEG2PH = 0x2;
+  C1CFG2bits.PRSEG = 0x1;
+  C1CFG2bits.SAM = 0x1;
+
+  /* 4 messages buffered in DMA RAM */
+  C1FCTRLbits.DMABS = 0;
+
+  /* dummy filter */
+  C1CTRL1bits.WIN = 1;
+  /* select acceptance mask 0 filter 0 buffer 1 */
+  C1FMSKSEL1bits.F0MSK = 0;
+
+  /* accept all standard sids */
+  C1RXM0SID = 0;
+  C1RXF0SID = 0;
+  C1RXM0SID = 0x0008; /* mide bit */
+  C1RXF0SID = 0;
+
+  /* use buffer 1 for incoming messages */
+  C1BUFPNT1bits.F0BP = 1;
+
+  /* enable filter 0 */
+  C1FEN1bits.FLTEN0 = 1;
+
+  /* clear window bit to access ECAN control registers */
+  C1CTRL1bits.WIN = 0;
+
+  /* put the module in normal mode */
+  C1CTRL1bits.REQOP = 0;
+  while (C1CTRL1bits.OPMODE != 0) ;
+
+  C1RXFUL1 = 0;
+  C1RXFUL2 = 0;
+  C1RXOVF1 = 0;
+  C1RXOVF2 = 0;
+
+  /* buffer N a transmit buffer */
+  C1TR01CONbits.TXEN0 = 1;
+  C1TR01CONbits.TXEN1 = 0;
+  C1TR23CONbits.TXEN2 = 0;
+  C1TR23CONbits.TXEN3 = 0;
 }
 
 static void ecan_write(uint8_t* s)
 {
+#define ecan_tx_buf (ecan_bufs[0])
+
+  /* wait for previous transmission to end */
+  while (C1TR01CONbits.TXREQ0) ;
+
+#define HOST_NODE_ID 0x0000
+#define BOOT_GROUP_ID 0x0000
+#define LOW_PRIO_ID 0x0000
+#define MAKE_CAN_ID(__p, __g, __n) (((__p) << 8) | ((__g) << 5) | __n)
+  ecan_tx_buf[0] = MAKE_CAN_ID( LOW_PRIO_ID, BOOT_GROUP_ID, HOST_NODE_ID );
+
+  ecan_tx_buf[1] = 0;
+#define CAN_DATA_SIZE 8
+  ecan_tx_buf[2] = CAN_DATA_SIZE;
+
+  ecan_tx_buf[3] = ((uint16_t)s[1] << 8) | s[0];
+  ecan_tx_buf[4] = ((uint16_t)s[3] << 8) | s[2];
+  ecan_tx_buf[5] = ((uint16_t)s[5] << 8) | s[4];
+  ecan_tx_buf[6] = ((uint16_t)s[7] << 8) | s[6];
+
+  C1TR01CONbits.TXREQ0 = 1;
+}
+
+static inline unsigned int ecan_is_rx(void)
+{
+  /* return 0 if no rx buffer full */
+
+  if (C1RXFUL1bits.RXFUL1) return 1;
+  else if (C1RXFUL1bits.RXFUL2) return 2;
+  else if (C1RXFUL1bits.RXFUL3) return 3;
+  return 0;
 }
 
 static void ecan_read(uint8_t* s)
 {
+  unsigned int buf_index;
+
+  while ((buf_index = ecan_is_rx()) == 0) ;
+
+#define ecan_rx_buf (ecan_bufs[buf_index])
+
+#if 0
+  *id = (ecan_rx_buf[0] & 0x1ffc) >> 2;
+#endif
+
+  s[0] = (uint8_t)ecan_rx_buf[3];
+  s[1] = (uint8_t)(ecan_rx_buf[3] >> 8);
+  s[2] = (uint8_t)ecan_rx_buf[4];
+  s[3] = (uint8_t)(ecan_rx_buf[4] >> 8);
+  s[4] = (uint8_t)ecan_rx_buf[5];
+  s[5] = (uint8_t)(ecan_rx_buf[5] >> 8);
+  s[6] = (uint8_t)ecan_rx_buf[6];
+  s[7] = (uint8_t)(ecan_rx_buf[6] >> 8);
+
+  if (buf_index == 1) C1RXFUL1bits.RXFUL1 = 0;
+  else if (buf_index == 2) C1RXFUL1bits.RXFUL2 = 0;
+  else if (buf_index == 3) C1RXFUL1bits.RXFUL3 = 0;
 }
 
 #define com_setup ecan_setup
